aa80f4559eb451df60268a84f42b9654011ca473
CouncilDataProjectServiceAccount <councildataproject@gmail.com>
2022-08-18 23:31:43 +0000

    Initial commit

---
+++ web/src/index.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { App, AppConfigProvider } from "@councildataproject/cdp-frontend";
+
+import "@councildataproject/cdp-frontend/dist/index.css";
+
+const config = {
+    firebaseConfig: {
+        options: {
+            projectId: "cdp-san-jose-5d9db455",
+        },
+        settings: {},
+    },
+    municipality: {
+        name: "San Jose",
+        timeZone: "America/Los_Angeles",
+        footerLinksSections: [],
+    },
+}
+
+ReactDOM.render(
+    <div>
+        <AppConfigProvider appConfig={config}>
+            <App />
+        </AppConfigProvider>
+    </div>,
+    document.getElementById("root")
+);
