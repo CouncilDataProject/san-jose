2692ccccb07f073387dcb72daf0cf525968785b5
Shak <shak.ragoler@gmail.com>
2023-05-08 21:40:48 -0700

    new method for identifying transcripts for deletion

--- python/clean_database.py
+++ python/clean_database.py
@@ -2,6 +2,7 @@ from cdp_backend.database import models as db_models
 import fireo
 from google.auth.credentials import AnonymousCredentials
 from google.cloud.firestore import Client
+import requests
 import sys
 
 # Connect to the database
@@ -10,28 +11,17 @@ fireo.connection(client=Client(
     credentials=AnonymousCredentials()
 ))
 
-# Read from the database
-sessions = db_models.Session.collection.fetch(1000)
-
-dictionary = dict()
 sessions_for_delete = []
 sessions = db_models.Session.collection.fetch(1000)
 for session in sessions:
-    if session.session_datetime not in dictionary:
-        dictionary[session.session_datetime] = []
-    dictionary[session.session_datetime].append(session)
-
-# Finding the sessions to delete
-for key in sorted(dictionary.keys()):
-    if len(dictionary[key]) > 1:
-        print(key)
-        for session in dictionary[key]:
-            if session.session_datetime.year == 2020 and session._update_time.year == 2022:
-                sessions_for_delete.append(session)
-                print(f"Session_time={str(session.session_datetime)} ID={str(session.id)} key={str(session.key)} update_time={str(session._update_time)} delete this from 2020")
-            else:
-                print(f"Session_time={str(session.session_datetime)} ID={str(session.id)} key={str(session.key)} update_time={str(session._update_time)}")
-        print("")
+    response = ""
+    try:
+        requests.head(session.video_uri)
+        print(session.video_uri)
+    except requests.exceptions.ConnectionError:
+        sessions_for_delete.append(session)
+        print(f"delete {session.video_uri}")
+print(len(sessions_for_delete))
 
 # This will delete the actual sessions
 if sys.argv[-1] == "real":
